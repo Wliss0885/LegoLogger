39ecc1c0930757cde5e8f0d4a5098c22dea02551
Chris Pham <chrispham238@gmail.com>
2026-03-11 10:48:00 -0400

    added some cool stuff

---
+++ Assets/Script/LegoSetController.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class LegoSetController : MonoBehaviour
+{
+    public String legoSetName;
+    public GameObject legoSet;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnLegoSetFound()
+    {
+        
+    }
+
+    public void OnLegoSetLost()
+    {
+        
+    }
+}
